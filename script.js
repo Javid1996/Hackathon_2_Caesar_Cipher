18a85a056d9f05250dc2f824a5e531f3a7802bea
Javid1996 <alcavid96@gmail.com>
2023-10-23 03:56:39 +0400

    encrypt

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 const encryptButton = document.getElementById('encrypt');
 const decryptButton = document.getElementById("decrypt");
 
-// encryptButton.addEventListener('click',encrypt)
+encryptButton.addEventListener('click',encrypt)
 decryptButton.addEventListener('click',decrypt)
 
 
-// function encrypt(){
-//     const input = document.getElementById("input_text");
-//     const output = document.getElementById("output_text");
-//     const key = document.getElementById('key');
-//     let encryptedText = "";
-//     let inputValue = input.value;
-//     let strInputValue = inputValue.toString()
-//     let keyValue=key.value;
-//     console.log(`Key  ${key}`);
-//     console.log(`Keyvalue ${keyValue}`);
-//     console.log(` INTPUT Val ${inputValue}`);
-//     console.log(`str INT Val ${strInputValue}`);
+function encrypt(){
+    const input = document.getElementById("input_text");
+    const output = document.getElementById("output_text");
+    const key = document.getElementById('key');
+    let encryptedText = "";
+    let inputValue = input.value;
+    let strInputValue = inputValue.toString()
+    let keyValue=key.value;
+    console.log(`Key  ${key}`);
+    console.log(`Keyvalue ${keyValue}`);
+    console.log(` INTPUT Val ${inputValue}`);
+    console.log(`str INT Val ${strInputValue}`);
 
-//     for(let i = 0; i<strInputValue.length;i++){
-//         const charCode = strInputValue.charCodeAt(i);
-//         if(charCode>=65 && charCode<=90){
-//             encryptedText+=String.fromCharCode(((charCode - 65 + keyValue)%26)+65);
-//         } 
-//         else if(charCode>=97 && charCode<=122){
-//             encryptedText+=String.fromCharCode(((charCode - 97 + keyValue)%26)+97);
-//         }
-//         else{
-//             encryptedText+=strInputValue[i];
-//         }
-//     }
+    for(let i = 0; i<strInputValue.length;i++){
+        const charCode = strInputValue.charCodeAt(i);
+        if(charCode>=65 && charCode<=90){
+            encryptedText+=String.fromCharCode(((charCode - 65 + keyValue)%26)+65);
+        } 
+        else if(charCode>=97 && charCode<=122){
+            encryptedText+=String.fromCharCode(((charCode - 97 + keyValue)%26)+97);
+        }
+        else{
+            encryptedText+=strInputValue[i];
+        }
+    }
 
-// output.value = encryptedText;
+output.value = encryptedText;
 
 
-// }
+}
 
 
 
